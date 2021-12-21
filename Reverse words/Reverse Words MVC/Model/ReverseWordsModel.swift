160d2567338d5ed9a414bd5ba7f98bb253fc8a3c
Artem Shcherban <artshcherban@gmail.com>
2021-12-21 15:11:35 +0200

    added Reverse Words app

---
+++ Reverse words/Reverse Words MVC/Model/ReverseWordsModel.swift
@@ -0,0 +1,15 @@
+//
+//  ReverseWorldModel.swift
+//  Reverse words
+//
+//  Created by Artem Shcherban on 16.12.2021.
+//
+
+import Foundation
+
+class ReverseWordsModel {
+
+    func reverseWords(_ string: String) -> String {
+        string.split(separator: " ").map({ "\($0)" }).map({ String($0.reversed()) }).joined(separator: " ")
+    }
+}
